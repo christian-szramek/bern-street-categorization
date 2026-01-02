4b64ad61dfaabdb7c0dcbf61bd8032ba6b4b15a1
Christian Szramek <christian.szramek@web.de>
2026-01-02 16:40:18 +0100

    Refactor download.sh

--- berlin.sh
+++ berlin.sh
@@ -2,5 +2,5 @@
 
 WORK_DIR=$(pwd)
 
-./import/prepare.sh $WORK_DIR Berlin https://download.geofabrik.de/europe/germany/berlin-latest.osm.pbf
+./download/download.sh $WORK_DIR Berlin https://download.geofabrik.de/europe/germany/berlin-latest.osm.pbf
 ./processing/processing.sh $WORK_DIR Berlin
--- bern.sh
+++ bern.sh
@@ -2,5 +2,5 @@
 
 WORK_DIR=$(pwd)
 
-./import/prepare.sh $WORK_DIR Bern https://download.geofabrik.de/europe/switzerland-latest.osm.pbf https://www.openstreetmap.org/api/0.6/relation/1682378/full
+./download/download.sh $WORK_DIR Bern https://download.geofabrik.de/europe/switzerland-latest.osm.pbf https://www.openstreetmap.org/api/0.6/relation/1682378/full
 ./processing/processing.sh $WORK_DIR Bern
