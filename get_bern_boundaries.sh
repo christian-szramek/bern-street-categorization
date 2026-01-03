2dc649d8fe4c40d70414388e6a65776b26fce425
Christian Szramek <christian.szramek@web.de>
2026-01-03 10:04:52 +0100

    Cleanup

--- get_bern_boundaries.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-curl https://www.openstreetmap.org/api/0.6/relation/1682378/full -o bern-boundaries.osm
--- get_bern_osm_file.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-osmium extract -p bern-boundaries.osm switzerland-251111.osm.pbf -o bern.osm.pbf
