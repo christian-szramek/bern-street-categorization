29e20666a1afff23777da36c77cacf5caba8138a
Christian Szramek <christian.szramek@web.de>
2026-01-11 12:02:44 +0100

    Make install_dependencies.sh executable

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# for process
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install osmium-tool -y
 sudo apt install osm2pgsql -y
+
+# for frontend 
+# Node.js v24.12.0 (npm v11.6.2)
