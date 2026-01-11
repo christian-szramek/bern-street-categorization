3d430f18f66829e100942c5a3553e10a878db026
Christian Szramek <christian.szramek@web.de>
2026-01-11 12:03:18 +0100

    Update frontend to the new table layout and display also nodes

--- frontend/src/components/Map.vue
+++ frontend/src/components/Map.vue
@@ -5,8 +5,10 @@ import { onMounted } from "vue";
 
 let map;
 
+const bern = [46.9481, 7.4474];
+
 onMounted(() => {
-  map = L.map("map").setView([46.9481, 7.4474], 13);
+  map = L.map("map").setView(bern, 13);
 
   L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
@@ -14,64 +16,59 @@ onMounted(() => {
       '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   }).addTo(map);
 
+  // nodes
+  L.vectorGrid
+    .protobuf("http://localhost:7800/public.highway_nodes/{z}/{x}/{y}.pbf", {
+      vectorTileLayerStyles: {
+        "public.highway_nodes": (properties, zoom) => {
+          return {
+            radius: 4, // circle radius in pixels
+            fillColor: "blue",
+            color: "blue",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 1,
+          };
+        },
+      },
+      interactive: true,
+      getFeatureId: f => f.properties.id,
+      maxZoom: 19,
+    })
+    .addTo(map);
+
   // ways
   L.vectorGrid
-    .protobuf(
-      "http://localhost:7800/public.street_segments_ways/{z}/{x}/{y}.pbf",
-      {
-        vectorTileLayerStyles: {
-          "public.street_segments_ways": {
-            stroke: true,
-            color: "green",
-            weight: 2,
-            opacity: 1,
-            interactive: true,
-            getFeatureId: f => f.properties.id,
-          },
+    .protobuf("http://localhost:7800/public.highway_ways/{z}/{x}/{y}.pbf", {
+      vectorTileLayerStyles: {
+        "public.highway_ways": {
+          stroke: true,
+          color: "green",
+          weight: 2,
+          opacity: 1,
+          interactive: true,
+          getFeatureId: f => f.properties.id,
         },
-        maxZoom: 19,
-      }
-    )
+      },
+      maxZoom: 19,
+    })
     .addTo(map);
 
-  // closed ways
+  // areas
   L.vectorGrid
-    .protobuf(
-      "http://localhost:7800/public.street_segments_closed_ways/{z}/{x}/{y}.pbf",
-      {
-        vectorTileLayerStyles: {
-          "public.street_segments_closed_ways": {
-            stroke: true,
-            color: "green",
-            weight: 2,
-            opacity: 1,
-            interactive: true,
-            getFeatureId: f => f.properties.id,
-          },
+    .protobuf("http://localhost:7800/public.highway_areas/{z}/{x}/{y}.pbf", {
+      vectorTileLayerStyles: {
+        "public.highway_areas": {
+          area: true,
+          color: "red",
+          weight: 2,
+          opacity: 1,
+          interactive: true,
+          getFeatureId: f => f.properties.id,
         },
-        maxZoom: 19,
-      }
-    )
-    .addTo(map);
-
-  // multiploygon
-  L.vectorGrid
-    .protobuf(
-      "http://localhost:7800/public.street_segments_multipolygon/{z}/{x}/{y}.pbf",
-      {
-        vectorTileLayerStyles: {
-          "public.street_segments_multipolygon": {
-            area: true,
-            color: "green",
-            weight: 2,
-            opacity: 1,
-            interactive: true,
-            getFeatureId: f => f.properties.id,
-          },
-        },
-        maxZoom: 19,
-      }
-    )
+      },
+      maxZoom: 19,
+    })
     .addTo(map);
 });
 </script>
