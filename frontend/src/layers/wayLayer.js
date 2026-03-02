887f1d8fb9c7dc0dc9936753ed75cf1f3b3267a5
Christian Szramek <christian.szramek@web.de>
2026-03-02 15:12:42 +0100

    Draw lanes as weight

--- frontend/src/layers/wayLayer.js
+++ frontend/src/layers/wayLayer.js
@@ -1,11 +1,32 @@
 const baseURL = "http://localhost:7800";
 
+const computeWeight = infrastructureType => {
+  switch (infrastructureType.split("_")[0]) {
+    case "2":
+      return 3;
+    case "3":
+      return 5;
+    case "4":
+      return 7;
+    case "5":
+      return 9;
+    case "6":
+      return 1;
+    case "7":
+      return 13;
+    case "8":
+      return 15;
+    default:
+      return 1;
+  }
+};
+
 const getWayLayer = (infrastructureType, color, handleMouseOver, minZoom) => {
   const styles = {};
-  styles[`public.${infrastructureType}_ways`] = (_, zoom) => ({
+  styles[`public.${infrastructureType}_ways`] = _ => ({
     stroke: true,
     color: color,
-    weight: zoom <= 14 ? 1 : 2,
+    weight: computeWeight(infrastructureType),
     opacity: 1,
   });
 
