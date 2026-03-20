a5786cc35ef2b9ed7279de7803d42cd3554dfa91
Christian Szramek <christian.szramek@web.de>
2026-03-20 11:32:07 +0100

    Add Jupyter Notebook

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -6,11 +6,11 @@ sudo apt install osmium-tool -y
 sudo apt install osm2pgsql -y
 
 # for process testing
-sudo apt install lua5.3
-sudo apt install liblua5.3-dev
-sudo apt install luarocks
-sudo apt install libexpat1-dev
-luarocks install luaexpat
+sudo apt install lua5.3 -y
+sudo apt install liblua5.3-dev -y
+sudo apt install luarocks -y
+sudo apt install libexpat1-dev -y
+luarocks install luaexpat -y
 # Add eval "$(luarocks path)" to .bashrc and source it
 
 # for frontend 
@@ -18,3 +18,8 @@ sudo apt install curl -y
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
 nvm use 25
 source ~/.bashrc
+
+# for Python Jupyter Notebook
+sudo apt install python3 -y
+sudo apt install python3-pip -y
+sudo apt install python3-venv -y
